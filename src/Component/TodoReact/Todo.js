188055bdc6b031046d140db0d8cb8863809ed215
rahulcdev13 <rahulc.dev13@gmail.com>
2022-05-27 19:48:52 +0530

    Finallt Todo List Done

--- src/Component/TodoReact/Todo.js
+++ src/Component/TodoReact/Todo.js
@@ -1,41 +1,83 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Style.css";
 import 'font-awesome/css/font-awesome.min.css';
- 
+
+//get localstorage data 
+const getLocaldata = () => {
+  const lists = localStorage.getItem("mytodolist");
+  if (lists) {
+    return JSON.parse(lists);
+  } else {
+    return [];
+  }
+};
+
 
 const Todo = () => {
   const [totoData, setTotoData] = useState("");
-  const [addData, setAddData] = useState([]);
+  const [addData, setAddData] = useState(getLocaldata());
+  const [editTodoItem, setEditItem] = useState("");
+  const [toggleButton, settoggleButton] = useState(false);
 
   //Add the item function
   const AddList = () => {
     if (!totoData) {
       alert('please fill the data')
-    } else {
-        const myNewInputData = {
+    } else if (totoData && toggleButton) {
+      setAddData(
+        addData.map((currVal)=>{
+          if(currVal.id===editTodoItem){
+            return {...currVal,name:totoData}
+          }
+          return currVal;
+        })
+      );
+      setTotoData("");
+    setEditItem(null);
+    settoggleButton(false);
+    }
+    else {
+      const myNewInputData = {
         id: new Date().getTime().toString(),
         name: totoData,
-      } 
+      }
       setAddData([...addData, myNewInputData]);
       setTotoData("");
     }
   }
   //How to delete items 
-  const deleteItem =(index)=>{
-    const undateItem = addData.filter((currVal)=>{
+  const deleteItem = (index) => {
+    const undateItem = addData.filter((currVal) => {
       return currVal.id !== index;
     })
-    setAddData(undateItem); 
-}
-const RemoveAll =()=>{
-  setAddData([]);
-}
- 
+    setAddData(undateItem);
+  }
+
+  //how to edit item
+  const editItem = (index) => {
+    const edititem = addData.find((currVal) => {
+      return currVal.id === index;
+    });
+    setTotoData(edititem.name)
+    setEditItem(index);
+    settoggleButton(true);
+  };
+
+  //Remove all the element
+  const RemoveAll = () => {
+    setAddData([]);
+  };
+
+  // How to Store and set on localStorage data 
+  useEffect(() => {
+    localStorage.setItem("mytodolist", JSON.stringify(addData))
+  }, [addData])
+
 
   return (
     <div className='main-div'>
       <div className='chile-div'>
-        <figure> 
+        <figure>
           <p style={{ fontSize: "150px" }}>📝</p>
           <figcaption>Add Your ToDo List Here ✌</figcaption>
         </figure>
@@ -46,7 +88,9 @@ const RemoveAll =()=>{
             onChange={(event) => setTotoData(event.target.value)}
             value={totoData}
           />
-          <i className="fa fa-plus add-btn" onClick={AddList}></i>
+
+          {settoggleButton ? <i className="fa fa-plus" style={{ fontSize: "15px" }} onClick={AddList}></i> : <i className="fa fa-edit add-btn" onClick={AddList}></i>}
+          {/* <i className="fa fa-plus add-btn" onClick={AddList}></i> */}
 
           {/* show our Items */}
           <div className='showItems'>
@@ -56,12 +100,12 @@ const RemoveAll =()=>{
                 <div className='eachItem' key={currVal.id} >
                   <h2>{currVal.name}</h2>
                   <div className='todo-btn'>
-                    <i className="fa fa-edit" style={{ fontSize: "15px" }}></i>
+                    <i className="fa fa-edit" style={{ fontSize: "15px" }} onClick={() => editItem(currVal.id)}></i>
                     <i className="fa fa-trash" style={{ fontSize: "15px" }} onClick={() => deleteItem(currVal.id)}></i>
                   </div>
                 </div>
               )
-            })} 
+            })}
 
           </div>
 
